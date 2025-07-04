c73a1790d3603d44ed801e6c3cf5587600549590
Brice LAURENT <kaszlukb@gmail.com>
2025-07-04 18:15:44 +0200

    remove session provider from auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,7 @@
-import { SessionProvider } from "next-auth/react"
-
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="p-4">
-          <SessionProvider>
-            <main>{children}</main>
-          </SessionProvider>
+      <main>{children}</main>
     </section>
   )
 }
