c96477c51301b35a8928b03a94bacd8ecc4706b5
Brice LAURENT <kaszlukb@gmail.com>
2025-10-31 10:19:13 +0100

    modify auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,20 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    const { data: session, status } = useSession();
+
+  // ⏳ Pendant le chargement
+  if (status === "loading") {
+    return <p>Chargement de la session...</p>;
+  }
+
+  // ❌ Si l'utilisateur n'est pas connecté
+  if (status === "unauthenticated" || !session) {
+    redirect("/"); // ou return <p>Non autorisé</p>
+  }
+
   return (
     <section className="p-4">
       <main>{children}</main>
--- src/app/auth/tableau-de-bord/layout.tsx
+++ src/app/auth/tableau-de-bord/layout.tsx
@@ -1,19 +1,4 @@
-"use client";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-
 export default function Dashboard({ children }: { children: React.ReactNode }) {
-  const { data: session, status } = useSession();
-
-  // ⏳ Pendant le chargement
-  if (status === "loading") {
-    return <p>Chargement de la session...</p>;
-  }
-
-  // ❌ Si l'utilisateur n'est pas connecté
-  if (status === "unauthenticated" || !session) {
-    redirect("/"); // ou return <p>Non autorisé</p>
-  }
 
   // ✅ Si connecté
   return <div>{children}</div>;
